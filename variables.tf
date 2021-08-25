686431cb3cc61ca45c917e8da71f6bf28bec0522
Anbazhagan <51202045+gitanbu@users.noreply.github.com>
2021-08-25 14:09:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "access_key" {
-	default = "YOUR_ACCESS_KEY"
+	default = "AKIA2LWN4LMSXPFJUUOU"
 }
 variable "secret_key" {
-	default = "YOUR_SECRET_KEY"
+	default = "hkItgp3XHuOiTZ1SQWh0g8rwXd+xstGojv5IM1GW"
 }
 variable "region" {
-    default = "your-region"
+    default = "us-west-2"
 }
